eadb927a54b0dce5b3aecc46dbf3d9341fd9b77a
pisc9211 <pisc9211@gmail.com>
2019-09-30 14:54:11 -0400

    added onchange handlers to inputs in register and signin pages

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Link } from 'react-router-dom'
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const update = {
+    email: setEmail,
+    password: setPassword,
+    firstName: setFirstName,
+    lastName: setLastName
+  };
+
+  const handleOnChange = e => update[e.target.id](e.target.value);
+
   return (
     <div className="container vh-100">
       <div className="row align-items-center h-100">
@@ -10,20 +24,20 @@ const Register = () => {
           <div className="py-5 mb-3">
             <h2 className="text-center text-uppercase">Register</h2>
             <div className="form-label-group my-3">
-              <label for="inputFirstName" className="mb-1 pl-2">First Name</label>
-              <input type="email" id="inputFirstName" className="form-control" placeholder="First Name" />
+              <label htmlFor="firstName" className="mb-1 pl-2">First Name</label>
+              <input value={firstName} onChange={handleOnChange} type="text" id="firstName" className="form-control" placeholder="First Name" required />
             </div>
             <div className="form-label-group my-3">
-              <label for="inputLastName" className="mb-1 pl-2">Last Name</label>
-              <input type="email" id="inputLastName" className="form-control" placeholder="Last Name" />
+              <label htmlFor="lastName" className="mb-1 pl-2">Last Name</label>
+              <input value={lastName} onChange={handleOnChange} type="text" id="lastName" className="form-control" placeholder="Last Name" required />
             </div>
             <div className="form-label-group my-3">
-              <label for="inputEmail" className="mb-1 pl-2">Email</label>
-              <input type="email" id="inputEmail" className="form-control" placeholder="Email Address" />
+              <label htmlFor="email" className="mb-1 pl-2">Email</label>
+              <input value={email} onChange={handleOnChange} type="email" id="email" className="form-control" placeholder="Email Address" required />
             </div>
             <div className="form-label-group my-3">
-              <label for="inputPassword" className="mb-1 pl-2">Email</label>
-              <input type="email" id="inputPassword" className="form-control" placeholder="Password" />
+              <label htmlFor="password" className="mb-1 pl-2">Password</label>
+              <input value={password} onChange={handleOnChange} type="password" id="password" className="form-control" placeholder="Password" required />
             </div>
             <button type="submit" className="btn btn-primary d-block w-100 mt-5">Register</button>
             <Link to="/" className="btn btn-primary d-block w-100 mt-3">Sign In</Link>
--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Link } from 'react-router-dom'
 
 const SignIn = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const update = {
+    email: setEmail,
+    password: setPassword
+  }
+
+  const handleOnChange = e => update[e.target.id](e.target.value);
+
   return (
     <div className="container vh-100">
       <div className="row align-items-center h-100">
@@ -10,12 +20,12 @@ const SignIn = () => {
           <div className="py-5 mb-3">
             <h2 className="text-center text-uppercase">Sign In</h2>
             <div className="form-label-group my-3">
-              <label for="inputEmail" className="mb-1 pl-2">Email</label>
-              <input type="email" id="inputEmail" className="form-control" placeholder="Email Address" />
+              <label htmlFor="email" className="mb-1 pl-2">Email</label>
+              <input value={email} onChange={handleOnChange} type="email" id="email" className="form-control" placeholder="Email Address" />
             </div>
             <div className="form-label-group my-3">
-              <label for="inputPassword" className="mb-1 pl-2">Email</label>
-              <input type="email" id="inputPassword" className="form-control" placeholder="Password" />
+              <label htmlFor="password" className="mb-1 pl-2">Password</label>
+              <input value={password} type="password" id="password" className="form-control" placeholder="Password" />
             </div>
             <button type="submit" className="btn btn-primary d-block w-100 mt-5">Sign In</button>
             <Link to="/register" type="button" className="btn btn-primary d-block w-100 mt-3">Register</Link>
